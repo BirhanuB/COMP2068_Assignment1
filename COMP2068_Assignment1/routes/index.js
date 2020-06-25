28bc5489f4e469033809633f9f5ebefad3350d2b
BirhanuB <55416464+BirhanuB@users.noreply.github.com>
2020-06-25 01:46:34 -0400

    Created site structure and added the required pages

--- COMP2068_Assignment1/routes/index.js
+++ COMP2068_Assignment1/routes/index.js
@@ -4,7 +4,27 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function (req, res) {
-    res.render('index', { title: 'Express' });
+    res.render('index', { title: 'Home' });
+});
+
+/* GET about me page. */
+router.get('/about', function (req, res) {
+    res.render('about', { title: 'About Me' });
+});
+
+/* GET projects page. */
+router.get('/projects', function (req, res) {
+    res.render('projects', { title: 'Projects' });
+});
+
+/* GET services page. */
+router.get('/services', function (req, res) {
+    res.render('services', { title: 'Services' });
+});
+
+/* GET contact me page. */
+router.get('/contact', function (req, res) {
+    res.render('contact', { title: 'Contact Me' });
 });
 
 module.exports = router;
